05f3eebe9a099e115c467a7528093bf948b8b5a4
RobinLiden <73289727+RobinLiden@users.noreply.github.com>
2021-10-20 16:19:54 +0200

    fixed more with parallax
    fixed more with parallax

--- Assets/PrototypeParallax/TestScripts/Parallax.cs
+++ Assets/PrototypeParallax/TestScripts/Parallax.cs
@@ -2,32 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
-{
-    public Camera cam;
-    public Transform subject;
+public class Parallax : MonoBehaviour {
 
-    Vector2 startPosition;
+    //Parallax Scroll Variables
+    public Camera cam;//the camera
+    public Transform subject;//the subject (usually the player character)
 
-    float startZ;
 
-    Vector2 Travel => (Vector2)cam.transform.position - startPosition;
+    //Instance variables
+    float zPosition;
+    Vector2 startPos;
 
-    float DistanceFromSubject => transform.position.z - subject.position.z;
 
-    float ClippingPlane => (cam.transform.position.z + (DistanceFromSubject > 0? cam.farClipPlane : cam.nearClipPlane));
+    //Properties
+    float twoAspect => cam.aspect * 2;
+    float tileWidth => (twoAspect > 3 ? twoAspect : 3);
+    float viewWidth => loopSpriteRenderer.sprite.rect.width / loopSpriteRenderer.sprite.pixelsPerUnit;
+    Vector2 travel => (Vector2)cam.transform.position - startPos; //2D distance travelled from our starting position
+    float distanceFromSubject => transform.position.z - subject.position.z;
+    float clippingPlane => (cam.transform.position.z + (distanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane));
+    float parallaxFactor => Mathf.Abs(distanceFromSubject) / clippingPlane;
 
-    float ParallaxFactor => Mathf.Abs(DistanceFromSubject) / ClippingPlane;
 
-    public void Start()
-    {
-        startPosition = transform.position;
-        startZ = transform.position.z;
+    //User options
+    public bool xAxis = true; //parallax on x?
+    public bool yAxis = true; //parallax on y?
+    public bool infiniteLoop = false; //are we looping?
+
+
+    //Loop requirement
+    public SpriteRenderer loopSpriteRenderer;
+
+
+    // Start is called before the first frame update
+    void Awake() {
+        cam = Camera.main;
+        startPos = transform.position;
+        zPosition = transform.position.z;
+
+        if (loopSpriteRenderer != null && infiniteLoop) {
+            float spriteSizeX = loopSpriteRenderer.sprite.rect.width / loopSpriteRenderer.sprite.pixelsPerUnit;
+            float spriteSizeY = loopSpriteRenderer.sprite.rect.height / loopSpriteRenderer.sprite.pixelsPerUnit;
+
+            loopSpriteRenderer.drawMode = SpriteDrawMode.Tiled;
+            loopSpriteRenderer.size = new Vector2(spriteSizeX * tileWidth, spriteSizeY);
+            transform.localScale = Vector3.one;
+        }
     }
 
-    public void Update()
-    {
-        Vector2 newPos = startPosition + Travel * ParallaxFactor;
-        transform.position = new Vector3(newPos.x, newPos.y, startZ);
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 newPos = startPos + travel * parallaxFactor;
+        transform.position = new Vector3(xAxis ? newPos.x : startPos.x, yAxis ? newPos.y : startPos.y, zPosition);
+
+        if (infiniteLoop) {
+            Vector2 totalTravel = cam.transform.position - transform.position;
+            float boundsOffset = (viewWidth / 2) * (totalTravel.x > 0 ? 1 : -1);
+            float screens = (int)((totalTravel.x + boundsOffset) / viewWidth);
+            transform.position += new Vector3(screens * viewWidth, 0);
+        }
     }
+
 }
