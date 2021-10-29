1d494053cb8fa2b8d2c3a149c1d94648369f4c2b
markusmiless98 <mikael.tagt@futuregames.nu>
2021-10-29 16:38:44 +0200

    Simple Push Of Small Details For Build
    Made checkpoint single use if we want

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    [SerializeField] bool isSingleUse;
+    bool hasBeenActivated;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if(collision.tag == "Player" && (!isSingleUse || !hasBeenActivated))
         {
+            hasBeenActivated = true;
             GameManager.Instance.SetCurrentCheckpoint(transform.position);
         }
     }
